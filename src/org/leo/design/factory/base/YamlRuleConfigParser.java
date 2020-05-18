4a4b72a7326c47b05511e5f9f0cac2d8c8c071a6
xiaochuang.mao <xiaochuang.mao@xiaoi.com>
2020-05-18 18:16:22 +0800

    feat: 工厂模式之简单工厂

---
+++ src/org/leo/design/factory/base/YamlRuleConfigParser.java
@@ -0,0 +1,16 @@
+package org.leo.design.factory.base;
+
+/**
+ * 模块名称: CRM
+ * 模块描述: TODO
+ *
+ * @author xiaochuang.mao
+ * @date 2020/5/18 12:59
+ */
+public class YamlRuleConfigParser implements IRuleConfigParser {
+    @Override
+    public RuleConfig parse(String configText) {
+        System.out.println("YamlRuleConfigParser...");
+        return new RuleConfig();
+    }
+}
